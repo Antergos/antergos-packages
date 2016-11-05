b402c741c3300a9b68ad0e0d7fe1b4d0d3b08e23
Dustin Falgout <dustin@falgout.us>
2016-11-05 11:44:50 -0500

    [updpkg] pamac-dev

--- antergos/jetbrains-toolbox/launcher.sh
+++ antergos/jetbrains-toolbox/launcher.sh
@@ -3,8 +3,11 @@
 
 _jbl_toolbox_user_dir="${HOME}/.local/share/JetBrains/Toolbox"
 _jbl_toolbox_system_dir='/opt/JetBrains/Toolbox'
+_jbl_user_version=''
+_jbl_system_version=''
 
 [[ -d  "${_jbl_toolbox_user_dir}" ]] && _jbl_is_first_run='false' || _jbl_is_first_run='true'
+[[ 'false' = "${_jbl_is_first_run}" ]] 
 
 
 is_first_run() {
@@ -17,7 +20,6 @@ maybe_create_symlinks() {
 	is_first_run || return 1
 
 	mkdir -p "${_jbl_toolbox_user_dir}"
-	ln -s "${_jbl_toolbox_system_dir}/bin" "${_jbl_toolbox_user_dir}/bin"
 	ln -s "${_jbl_toolbox_system_dir}/.settings.json" "${_jbl_toolbox_user_dir}/.settings.json"
 	ln -s /usr/share/pixmaps/jetbrains-toolbox.svg "${_jbl_toolbox_user_dir}/toolbox.svg"
 
@@ -25,8 +27,46 @@ maybe_create_symlinks() {
 }
 
 
+versions_match() {
+	_jbl_user_version=$(<"${_jbl_toolbox_user_dir}/.installed_version")
+	_jbl_system_version=$(<"${_jbl_toolbox_system_dir}/.installed_version")
 
-maybe_create_symlinks
+	[[ "${_jbl_user_version}" = "${_jbl_system_version}" ]] && return 0
+
+	return 1
+}
+
+
+is_first_run_after_upgrade() {
+	{ is_first_run || versions_match; } && return 1
+
+	return 0
+}
+
+
+maybe_remove_symlink_to_binary() {
+	is_first_run_after_upgrade || return 1
+
+	unlink "${_jbl_toolbox_user_dir}/bin"
+
+	return 0
+}
+
+
+maybe_move_binary() {
+	is_first_run && return 1
+	[[ -h "${_jbl_toolbox_user_dir}/bin" ]] && return 1
+
+	cp -r "${_jbl_toolbox_user_dir}/bin" "${_jbl_toolbox_system_dir}"
+	rm -rf "${_jbl_toolbox_user_dir}/bin"
+	ln -s "${_jbl_toolbox_system_dir}/bin" "${_jbl_toolbox_user_dir}/bin"
+
+	
+}
+
+
+
+maybe_create_symlinks || maybe_move_binary
 
 if is_first_run; then
 	exec "${_jbl_toolbox_system_dir}/jetbrains-toolbox" "$@"
